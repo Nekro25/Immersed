16c186d0f3fb917088bb5ed8de3f0ef7f8d7825c
Amir <g.amir2004@gmail.com>
2021-12-26 14:37:57 +0300

    created gameplay and updated main

---
+++ gameplay.py
@@ -0,0 +1,22 @@
+import pygame
+import sys
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def game_circle():
+    pygame.init()
+    pygame.display.set_caption('Name')
+    size = width, height = 1250, 750
+    screen = pygame.display.set_mode(size)
+    running = True
+    clock = pygame.time.Clock()
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+        pygame.display.flip()
+    pygame.quit()
