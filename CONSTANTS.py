1a337865efe52c3a5a02c45ff147874fe001ea52
blxxdclxud <ramazanatzuf10@gmail.com>
2021-12-29 11:27:55 +0300

    new consts

--- CONSTANTS.py
+++ CONSTANTS.py
@@ -1,10 +1,15 @@
+import pygame
+
+
 WATER = 0
 FIRE = 1
 ELSE = 2
+PLAYER = 7
 
 BLUE = (0, 0, 255, 255)
 BLACK = (0, 0, 0, 0)
 RED = (255, 0, 0, 255)
+YELLOW = (255, 255, 0, 255)
 
 BLOCKS = {
     BLUE: WATER,
@@ -16,3 +21,5 @@ MAP_NAME = 'immersed in ice.png'
 WIDTH = 1250
 HEIGHT = 750
 FPS = 60
+
+clock = pygame.time.Clock()
