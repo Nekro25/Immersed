7905cae95130e564ccadcfe26d223dbfb1a15246
Amir <g.amir2004@gmail.com>
2022-01-07 13:19:09 +0300

    Animation added

--- entities.py
+++ entities.py
@@ -16,10 +16,30 @@ class Camera:
         self.y_coef = object.cell_y
 
 
+# class AnimatedSprite(pygame.sprite.Sprite):
+#     def __init__(self, sheet, columns, rows, x, y):
+#         super().__init__()
+#         self.frames = []
+#         cut_sheet(self, sheet, 5, BLOCK_SIZE)
+#         self.cur_frame = 0
+#         self.image = self.frames[self.cur_frame]
+#         self.rect = self.rect.move(x, y)
+#
+#     def update(self):
+#         self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+#         self.image = self.frames[self.cur_frame]
+
+
 class Player(pygame.sprite.Sprite):
     def __init__(self, y, x, *args):
         super().__init__(*args)
+        self.frames = []
+        self.get_frames(PLAYER_ANIMATION_img, 5, 50)
+        self.cur_frame_num = 0
+        self.cur_frame = None
         self.image = PLAYER_img
+        self.animate_event = pygame.USEREVENT + 2
+        pygame.time.set_timer(self.animate_event, SECOND // 10)
         self.rect = self.image.get_rect()
         self.rect.x = 0
         self.rect.y = 0
@@ -30,6 +50,44 @@ class Player(pygame.sprite.Sprite):
         self.cell_y = 0
         self.map_x = int(x) * BLOCK_SIZE  # Позиция относительно всей карты
         self.map_y = int(y) * BLOCK_SIZE
+
+    def get_frames(self, sheet, length, size):
+        self.rect = pygame.Rect(0, 0, size, size)
+        for i in range(length):
+            self.frames.append(sheet.subsurface((size * i, 0, size, size)))
+        self.frames_45 = [pygame.transform.rotate(i, 45) for i in self.frames]
+        self.frames_90 = [pygame.transform.rotate(i, 90) for i in self.frames]
+        self.frames_315 = [pygame.transform.rotate(i, 315) for i in self.frames]
+        self.frames_180 = [pygame.transform.flip(i, True, False) for i in self.frames]
+        self.frames_135 = [pygame.transform.flip(i, True, False) for i in self.frames_45]
+        self.frames_225 = [pygame.transform.flip(i, True, False) for i in self.frames_315]
+        self.frames_275 = [pygame.transform.flip(i, True, True) for i in self.frames_90]
+
+    def update(self, dict):
+        if any(dict.values()):
+            if dict[pygame.K_w] and dict[pygame.K_a]:
+                self.cur_frame = self.frames_315
+            elif dict[pygame.K_w] and dict[pygame.K_d]:
+                self.cur_frame = self.frames_225
+            elif dict[pygame.K_s] and dict[pygame.K_a]:
+                self.cur_frame = self.frames_45
+            elif dict[pygame.K_s] and dict[pygame.K_d]:
+                self.cur_frame = self.frames_135
+            elif dict[pygame.K_w]:
+                self.cur_frame = self.frames_275
+            elif dict[pygame.K_a]:
+                self.cur_frame = self.frames
+            elif dict[pygame.K_s]:
+                self.cur_frame = self.frames_90
+            elif dict[pygame.K_d]:
+                self.cur_frame = self.frames_180
+        else:
+            self.cur_frame = None
+        if self.cur_frame:
+            self.cur_frame_num = (self.cur_frame_num + 1) % 5
+            self.image = self.cur_frame[self.cur_frame_num]
+        else:
+            self.image = PLAYER_img
 
     # движение игрока в пространстве
     def move_up(self, group):
