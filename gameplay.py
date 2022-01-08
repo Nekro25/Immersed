2c9f2a3275b390eef6a7364e7047ae4116c888aa
Amir <g.amir2004@gmail.com>
2022-01-09 00:07:48 +0300

    bugs fixed

--- gameplay.py
+++ gameplay.py
@@ -250,7 +250,7 @@ def game_loop():
         #  проверка смерти
         if lifebar.health_lvl < 0 or lifebar.oxygen_lvl < 0:
             new_save(player.y, player.x, lifebar.oxygen_lvl, lifebar.health_lvl, progress,
-                     1)
+                     1, game_statistics)
             pos, ox, hp, progress = end_screen(screen)
             player = Creature(*pos, PLAYER_img, PLAYER_ANIMATION_img, 5, 1, 50, 50)
             lifebar.oxygen_lvl = ox
